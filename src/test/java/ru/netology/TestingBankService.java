ad17a91b2bcb7a174632c46ca9d5b060e585d07a
Darya Kobenko <kobenko.dv@petretail.ru>
2020-03-09 18:13:01 +0300

    add selenide test and tested service

---
+++ src/test/java/ru/netology/TestingBankService.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class TestingBankService {
+    @Test
+    public void SendForm()
+    {
+        open("http://127.0.0.1:9999");
+        $("[name=\"name\"]").setValue("Петр Первый");
+        $("[name=\"phone\"]").setValue("+79518828046");
+        $("[data-test-id=\"agreement\"]").click();
+        $(".button_view_extra").click();
+
+        $("[data-test-id=\"order-success\"]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
