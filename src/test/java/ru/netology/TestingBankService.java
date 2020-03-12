0ed1848af2c91e910eab1a043a12e40bb1b383de
Darya Kobenko <kobenko.dv@petretail.ru>
2020-03-12 09:31:11 +0300

    correct some refs java name method - and add DisplayName annotation

--- src/test/java/ru/netology/TestingBankService.java
+++ src/test/java/ru/netology/TestingBankService.java
@@ -6,16 +6,17 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class TestingBankService {
-    @Test
-    public void SendForm()
+	@DisplayName("This we send form request")
+    @Tes
+    public void send_form()
     {
         open("http://127.0.0.1:9999");
-        $("[name=\"name\"]").setValue("Петр Первый");
-        $("[name=\"phone\"]").setValue("+79518828046");
-        $("[data-test-id=\"agreement\"]").click();
+        $("[name='name']").setValue("Петр Первый");
+        $("[name='phone']").setValue("+79518828046");
+        $("[data-test-id='agreement']").click();
         $(".button_view_extra").click();
 
-        $("[data-test-id=\"order-success\"]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
 }
